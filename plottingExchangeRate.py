b91cfdf7ba3fbb8c76595c2f911c1dfc61957a1c
Arjun Kundu <akundu8149@upei.ca>
2024-07-07 18:37:32 -0300

    Made progress in getting one graph to show. Now onto exploring subplots.

--- plottingExchangeRate.py
+++ plottingExchangeRate.py
@@ -29,14 +29,14 @@ with open('/Users/kunduarjun02/Desktop/VSCode/CurrencyConversion/trackingExchang
         index_of_CAD_conversion = line.index('1 CAD = ')
         index_of_EUR = line.index('EUR, ')
         CAD_conversion_value = line[index_of_CAD_conversion + 8: index_of_EUR - 1]
-        # Should this value be converted to an int before adding to the list? This shouldn't be necessary to my understanding. 
+        CAD_conversion_value = float(CAD_conversion_value) 
         CAD_to_EUR.append(CAD_conversion_value)
 
         # This snippet of code will capture the exchange rate of EUR to CAD on the date specified on the line. 
         index_of_EUR_conversion = line.index('1 EUR = ')
         index_of_CAD = line.index('CAD.')
         EUR_conversion_value = line[index_of_EUR_conversion + 8: index_of_CAD - 1]
-        # Should this value be converted to an int before adding to the list? This shouldn't be necessary to my understanding.
+        EUR_conversion_value = float(EUR_conversion_value)
         EUR_to_CAD.append(EUR_conversion_value)
 
 
@@ -59,3 +59,13 @@ if data_points_to_view > total_values:
 dates_to_map = dates[-data_points_to_view:]
 CAD_to_EUR_to_map = CAD_to_EUR[-data_points_to_view:]
 EUR_to_CAD_to_map = EUR_to_CAD[-data_points_to_view:]
+
+plt.plot(dates_to_map, CAD_to_EUR_to_map, label='1 CAD equals this many EUR')
+# plt.plot(dates_to_map, EUR_to_CAD_to_map, label='1 EUR equals this many CAD')
+plt.xlabel('Date')
+plt.xticks(rotation = 45)
+plt.ylabel('Exchange Rates')
+plt.title('Time Lapse for CAD and EUR Exchange Rates')
+plt.legend()
+plt.tight_layout()
+plt.show()
